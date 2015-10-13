0389112d8c3963672b27d1edf1bc56ce9a62433d
iamkevingreen <thedudem4n@gmail.com>
2015-10-13 07:23:30 -0400

    remove the bower stuff that we're not being used, create the login controller

---
+++ client/controllers/authentication/login.js
@@ -0,0 +1,45 @@
+/*
+  Controller: Login
+  Template: /client/views/registration/login.html
+*/
+
+Template.login.rendered = function() {
+  return $('#login').validate({
+    rules: {
+      emailAddress: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true
+      }
+    },
+    messages: {
+      emailAddress: {
+        required: "Please enter your email address to login.",
+        email: "Please enter a valid email address."
+      },
+      password: {
+        required: "Please enter your password to login."
+      }
+    },
+    submitHandler: function() {
+      var user;
+      user = {
+        email: document.querySelector('[name="emailAddress"]').value,
+        password: document.querySelector('[name="password"]').value
+      };
+      return Meteor.loginWithPassword(user.email, user.password, function(error) {
+        if (error) {
+          return alert(error.reason);
+        }
+      });
+    }
+  });
+}
+
+Template.login.events({
+  'submit form': function(event, template) {
+    return event.preventDefault();
+  }
+});
