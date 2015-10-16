f440ccac1fb1d622ecf923ac35f4bc92bd31443f
iamkevingreen <thedudem4n@gmail.com>
2015-10-15 20:53:29 -0400

    render the team members on the home page that are created from the backend. aka spaghetti

--- client/react-views/home/teams.jsx
+++ client/react-views/home/teams.jsx
@@ -1,12 +1,36 @@
 Teams = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    return {
+      members: Members.find({}).fetch(),
+    }
+  },
+  renderMembers() {
+    return this.data.members.map((member) => {
+      return <TeamMember member={member} />;
+    });
+  },
   render() {
     return (
       <div className="">
-        <p>testing</p>
+        <p>{this.renderMembers()}</p>
       </div>
     )
   }
 });
+
+TeamMember = React.createClass({
+  propTypes: {
+    member: React.PropTypes.object.isRequired
+  },
+  render() {
+    return (
+      <li>
+        <span className="member">{this.props.member.name}</span>
+      </li>
+    )
+  }
+})
 
 Template.home.helpers({
   Teams() {
